ac6a5f23967cb887ecac7aa94f668c08ae478d2a
Duarte Duarte <dnpd.dd@gmail.com>
2014-03-05 16:17:49 +0000

    Load pages asynchronously with Ajax

--- js/plugins/AdminLTE/app.js
+++ js/plugins/AdminLTE/app.js
@@ -5,8 +5,31 @@
  *      This file should be included in all pages
  !**/
 
+function hash_change() {
+    if (document.location.hash.length === 0)
+        document.location.hash = "page_catalog"; // default page
+    var hash_parts = document.location.hash.slice(1).split("&"); // first character is #
+
+    hash_parts.forEach(function (hash) {
+        var pattern = /([A-Za-z]+)_([A-Za-z]+)/; // name_value
+        var result = pattern.exec(hash);
+        if (result[1] === "page")
+            load_ajax(result[2]);
+    });
+}
+
+function load_ajax(name) {
+    $("#content-ajax").load("pages/" + name + ".html", function () {
+        var titleElem = $("#content-ajax>title");
+        document.title = titleElem.text() + " | Socializ3d";
+        titleElem.remove();
+    });
+}
+
 $(function() {
     "use strict";
+
+    window.onhashchange = hash_change;
 
     //Enable sidebar toggle
     $("[data-toggle='offcanvas']").click(function(e) {
@@ -123,62 +146,11 @@ $(function() {
         checkboxClass: 'icheckbox_minimal',
         radioClass: 'iradio_minimal'
     });
-
-    /* For demo purposes */
-    var demo = $("<div />").css({
-        position: "fixed",
-        top: "150px",
-        right: "0",
-        background: "rgba(0, 0, 0, 0.7)",
-        "border-radius": "5px 0px 0px 5px",
-        padding: "10px 15px",
-        "font-size": "16px",
-        "z-index": "999999",
-        cursor: "pointer",
-        color: "#ddd"
-    }).html("<i class='fa fa-gear'></i>").addClass("no-print");
-
-    var demo_settings = $("<div />").css({
-        "padding": "10px",
-        position: "fixed",
-        top: "130px",
-        right: "-200px",
-        background: "#fff",
-        border: "3px solid rgba(0, 0, 0, 0.7)",
-        "width": "200px",
-        "z-index": "999999"
-    }).addClass("no-print");
-    demo_settings.append(
-            "<h4 style='margin: 0 0 5px 0; border-bottom: 1px dashed #ddd; padding-bottom: 3px;'>Layout Options</h4>"
-            + "<div class='form-group no-margin'>"
-            + "<div class='.checkbox'>"
-            + "<label>"
-            + "<input type='checkbox' onchange='change_layout();'/> "
-            + "Fixed layout"
-            + "</label>"
-            + "</div>"
-            + "</div>"
-            );
-
-    demo.click(function() {
-        if (!$(this).hasClass("open")) {
-            $(this).css("right", "200px");
-            demo_settings.css("right", "0");
-            $(this).addClass("open");
-        } else {
-            $(this).css("right", "0");
-            demo_settings.css("right", "-200px");
-            $(this).removeClass("open")
-        }
-    });
-
-    $("body").append(demo);
-    $("body").append(demo_settings);
 });
+
 function change_layout() {
     $("body").toggleClass("fixed");
 }
-/*END DEMO*/
 
 /* 
  * BOX REFRESH BUTTON 
