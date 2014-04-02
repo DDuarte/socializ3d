f72965e8ad8ef0b45841474826ca000a914344bc
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-04-02 18:29:11 +0100

    Add more queries to queries.sql.

--- sql/queries.sql
+++ sql/queries.sql
@@ -11,9 +11,9 @@ SELECT name, description, FROM Model WHERE Model.name = :name AND visibility = '
 
 ---
 
--------------------------
+------------------
 -- Gallery page --
--------------------------
+------------------
 	
 -- Thumbnail information for each model --
 SELECT DISTINCT Model.name as model, Member.name as author, count(CASE WHEN upVote = 'true' THEN 1 ELSE null END) as upVotes, count(CASE WHEN upVote = 'false' THEN 0 ELSE null END) as downVotes, 
@@ -23,4 +23,35 @@ SELECT DISTINCT Model.name as model, Member.name as author, count(CASE WHEN upVo
 	JOIN TComment ON TComment.idModel = Model.id
 	WHERE Model.id = :id
 	GROUP BY Model.name, Member.name, TComment.id;
+	
+-- List all the members of a group --
+SELECT Member.id, Member.name FROM GroupUser 
+	JOIN TGroup ON GroupUser.idGroup = :groupId
+	JOIN Member ON Member.id = GroupUser.idMember
+	ORDER BY Member.name ASC;
+	
+-- List all the administrators of a group --
+SELECT Member.id, Member.name FROM GroupUser 
+	JOIN TGroup ON GroupUser.idGroup = :groupId
+	JOIN Member ON (Member.id = GroupUser.idMember AND Member.isAdmin = 'true')
+	ORDER BY Member.name ASC;
+	
+-- List all the friends of a user --
+SELECT Member.id, Member.name FROM (SELECT FriendShip.idMember1 AS idFriend FROM Friendship WHERE Friendship.idMember2 = :id 
+	UNION
+	SELECT FriendShip.idMember2 AS idFriend FROM Friendship WHERE Friendship.idMember1 = :id) 
+	JOIN Member ON Member.id = idFriend;
+	
+-- List all the groups of a user --
+SELECT TGroup.id, TGroup.name FROM TGroup JOIN GroupUser ON GroupUser.idGroup = TGroup.id AND GroupUser.idMember = :id;
 
+-- List all the models of a group --
+SELECT DISTINCT Model.name as model, Member.name as author, count(CASE WHEN upVote = 'true' THEN 1 ELSE null END) as upVotes, count(CASE WHEN upVote = 'false' THEN 0 ELSE null END) as downVotes, 
+	count(TComment.id) as comments 
+	FROM Model
+	JOIN GroupModel ON (GroupModel.idGroup = :id AND GroupModel.idModel = Model.id)
+	JOIN Vote ON Model.id = Vote.idModel 
+	JOIN Member ON Member.id = Model.idAuthor 
+	JOIN TComment ON TComment.idModel = Model.id
+	GROUP BY Model.name, Member.name, TComment.id;
+-- 
