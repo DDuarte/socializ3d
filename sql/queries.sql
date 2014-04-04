1714c525f8528df398470b8c872bcd4c10f0f971
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-04-04 01:09:26 +0100

    Update queries.sql: replace parametrized queries with functions.

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,75 +1,116 @@
+DROP TRIGGER IF EXISTS insert_on_user_tags_view_trigger ON user_tags;
+DROP TRIGGER IF EXISTS delete_from_user_tags_view_trigger ON user_tags;
+DROP TRIGGER IF EXISTS insert_on_model_tags_view_trigger ON model_tags;
+DROP TRIGGER IF EXISTS delete_from_model_tags_view_trigger ON model_tags;
+
 -------------------------
 -- Search results page --
 -------------------------
 
--- Groups --
-SELECT name, about, avatarImg, createDate FROM TGroup WHERE visibility = 'public' AND TGroup.name = :groupName;
--- Users --
-SELECT name, about, avatarImg, registerDate FROM Member WHERE Member.name = :memberName;
--- Models --
-SELECT name, description, createDate FROM Model WHERE Model.name = :modelName AND visibility = 'public';
+-- TGroup --
+CREATE OR REPLACE FUNCTION get_groups_with_name(name varchar)
+RETURNS TABLE(name varchar, about varchar, avatarImg varchar, createDate timestamp) AS $$
+	SELECT name, about, avatarImg, createDate 
+	FROM TGroup WHERE visibility = 'public' AND TGroup.name = $1
+$$ LANGUAGE SQL;
+
+-- Member --
+CREATE OR REPLACE FUNCTION get_members_with_name(name varchar)
+RETURNS TABLE(name varchar, about varchar, registerDate timestamp) AS $$
+	SELECT name, about, registerDate 
+	FROM Member WHERE Member.name = $1
+$$ LANGUAGE SQL;
+
+-- Model --
+CREATE OR REPLACE FUNCTION get_models_with_name(name varchar)
+RETURNS TABLE(name varchar, description varchar, createDate timestamp) AS $$
+	SELECT name, description, createDate 
+	FROM Model WHERE Model.name = $1 AND visibility = 'public'
+$$ LANGUAGE SQL;
 
 ---
 
-------------------
--- Gallery page --
-------------------
-	
--- Thumbnail information for each model --
-SELECT DISTINCT Model.name as model, Member.name as author, count(CASE WHEN upVote = 'true' THEN 1 ELSE 0 END) as upVotes, count(CASE WHEN upVote = 'false' THEN 1 ELSE 0 END) as downVotes, count(TComment.id) as comments 
-	FROM Model 
-	JOIN Member ON Member.id = Model.idAuthor 
-    LEFT JOIN Vote ON Model.id = Vote.idModel 
-	LEFT JOIN TComment ON TComment.idModel = Model.id
-	WHERE Model.id = :id
-	GROUP BY Model.id, Member.id;
+-- Get thumbnail information for a model --
+CREATE OR REPLACE FUNCTION get_thumbnail_information(modelId bigint) 
+RETURNS TABLE(modelName varchar, authorName varchar, upVotes bigint, downVotes bigint, comments bigint) AS $$
+	SELECT DISTINCT Model.name as model, Member.name as author, count(CASE WHEN upVote = 'true' THEN 1 ELSE 0 END) as upVotes, count(CASE WHEN upVote = 'false' THEN 1 ELSE 0 END) as downVotes, count(TComment.id) as comments 
+		FROM Model 
+		JOIN Member ON Member.id = Model.idAuthor 
+		LEFT JOIN Vote ON Model.id = Vote.idModel 
+		LEFT JOIN TComment ON TComment.idModel = Model.id
+		WHERE Model.id = $1
+		GROUP BY Model.id, Member.id
+$$ LANGUAGE SQL;
 	
 -- List all the members of a group --
-SELECT Member.id, Member.name FROM GroupUser 
-	JOIN TGroup ON GroupUser.idGroup = :groupId
-	JOIN Member ON Member.id = GroupUser.idMember
-	ORDER BY Member.name ASC;
+CREATE OR REPLACE FUNCTION get_members_of_group(groupId bigint)
+RETURNS TABLE(memberId bigint, memberName varchar) AS $$
+	SELECT Member.id, Member.name FROM GroupUser 
+		JOIN TGroup ON GroupUser.idGroup = $1
+		JOIN Member ON Member.id = GroupUser.idMember
+		ORDER BY Member.name ASC
+$$ LANGUAGE SQL;
 	
 -- List all the administrators of a group --
-SELECT Member.id, Member.name FROM GroupUser 
-	JOIN TGroup ON GroupUser.idGroup = :groupId
-	JOIN Member ON (Member.id = GroupUser.idMember AND Member.isAdmin = 'true')
-	ORDER BY Member.name ASC;
-	
+CREATE OR REPLACE FUNCTION get_administrators_of_group(groupId bigint)
+RETURNS TABLE(memberId bigint, memberName varchar) AS $$
+	SELECT Member.id, Member.name FROM GroupUser 
+		JOIN TGroup ON GroupUser.idGroup = $1
+		JOIN Member ON (Member.id = GroupUser.idMember AND GroupUser.isAdmin = 'true')
+		ORDER BY Member.name ASC
+$$ LANGUAGE SQL;
+
 -- List all the friends of a user --
-SELECT Member.id, Member.name 
-FROM (
-    SELECT FriendShip.idMember1 AS idFriend FROM Friendship WHERE Friendship.idMember2 = :id 
-	UNION ALL
-	SELECT FriendShip.idMember2 AS idFriend FROM Friendship WHERE Friendship.idMember1 = :id
-) JOIN Member ON Member.id = idFriend;
+CREATE OR REPLACE FUNCTION get_friends_of_member(memberId bigint)
+RETURNS TABLE(memberId bigint, memberName varchar) AS $$
+	SELECT Member.id, Member.name 
+	FROM (
+		SELECT FriendShip.idMember1 AS idFriend FROM Friendship WHERE Friendship.idMember2 = $1
+		UNION ALL
+		SELECT FriendShip.idMember2 AS idFriend FROM Friendship WHERE Friendship.idMember1 = $1
+	) AS friends JOIN Member ON Member.id = idFriend
+$$ LANGUAGE SQL;
 	
 -- List all the groups of a user --
-SELECT TGroup.id, TGroup.name FROM TGroup JOIN GroupUser ON GroupUser.idGroup = TGroup.id AND GroupUser.idMember = :id;
+CREATE OR REPLACE FUNCTION get_groups_of_member(memberId bigint)
+RETURNS TABLE(groupId bigint, groupName varchar) AS $$
+	SELECT TGroup.id, TGroup.name 
+	FROM TGroup 
+	JOIN GroupUser ON GroupUser.idGroup = TGroup.id AND GroupUser.idMember = $1
+$$ LANGUAGE SQL;
 
 -- List all the models of a group --
-SELECT DISTINCT Model.name as model, Member.name as author, count(CASE WHEN upVote = 'true' THEN 1 ELSE null END) as upVotes, count(CASE WHEN upVote = 'false' THEN 0 ELSE null END) as downVotes, 
-	count(TComment.id) as comments 
-	FROM Model
-	JOIN GroupModel ON (GroupModel.idGroup = :id AND GroupModel.idModel = Model.id)
-	JOIN Vote ON Model.id = Vote.idModel 
-	JOIN Member ON Member.id = Model.idAuthor 
-	JOIN TComment ON TComment.idModel = Model.id
-	GROUP BY Model.name, Member.name, TComment.id;
+CREATE OR REPLACE FUNCTION get_models_of_group(groupId bigint)
+RETURNS TABLE(modelName varchar, authorName varchar, upVotes bigint, downVotes bigint, comments bigint) AS $$
+	SELECT DISTINCT Model.name as model, Member.name as author, count(CASE WHEN upVote = 'true' THEN 1 ELSE null END) as upVotes, count(CASE WHEN upVote = 'false' THEN 0 ELSE null END) as downVotes, count(TComment.id) as comments 
+		FROM Model
+		JOIN GroupModel ON (GroupModel.idGroup = $1 AND GroupModel.idModel = Model.id)
+		JOIN Vote ON Model.id = Vote.idModel 
+		JOIN Member ON Member.id = Model.idAuthor 
+		JOIN TComment ON TComment.idModel = Model.id
+		GROUP BY Model.id, Member.id
+$$ LANGUAGE SQL;
+
+-------------------
+-- Notifications --
+-------------------
 
 -- List the newest notifications within a given range --	
 CREATE OR REPLACE FUNCTION get_notifications(oldest_date_limit timestamp, max_notifications_limit integer) RETURNS TABLE(idNotification bigint, notType notification_type, idFriendshipInvite bigint, idGroupApplication bigint, idGroupInvite bigint, idModel bigint) AS $$
 	SELECT id, notificationType, idFriendshipInvite, idGroupApplication, idGroupInvite, idModel FROM Notification WHERE createDate < $1 LIMIT $2
 $$ LANGUAGE SQL;
 
+-- List the newest member notifications within a given range --
 CREATE OR REPLACE FUNCTION get_member_notifications(memberId bigint, oldest_date_limit timestamp, max_notifications_limit integer) RETURNS TABLE(idNotification bigint, notType notification_type, idFriendshipInvite bigint, idGroupApplication bigint, idGroupInvite bigint, idModel bigint) AS $$
 	SELECT Notification.id, Notification.notificationType, Notification.idFriendshipInvite, Notification.idGroupApplication, Notification.idGroupInvite, Notification.idModel FROM Notification JOIN UserNotification ON UserNotification.idMember = $1 AND UserNotification.idNotification = Notification.id  WHERE createDate < $2 LIMIT $3
 $$ LANGUAGE SQL;
 
+-- List the newest group notifications within a given range --
 CREATE OR REPLACE FUNCTION get_group_notifications(groupId bigint, oldest_date_limit timestamp, max_notifications_limit integer) RETURNS TABLE(idNotification bigint, notType notification_type, idFriendshipInvite bigint, idGroupApplication bigint, idGroupInvite bigint, idModel bigint) AS $$
 	SELECT Notification.id, Notification.notificationType, Notification.idFriendshipInvite, Notification.idGroupApplication, Notification.idGroupInvite, Notification.idModel FROM Notification JOIN GroupNotification ON GroupNotification.idGroup = $1 AND GroupNotification.idNotification = Notification.id  WHERE createDate < $2 LIMIT $3
 $$ LANGUAGE SQL;
 
+-- List the newest publications for a particular member --
 CREATE OR REPLACE FUNCTION get_model(memberId bigint, oldest_date_limit timestamp, max_notifications_limit integer) RETURNS TABLE(idModel bigint) AS $$
 	SELECT Model.id
     FROM Model 
@@ -87,7 +128,7 @@ $$ LANGUAGE SQL;
 -- Views --
 -----------
 
-CREATE VIEW user_tags AS
+CREATE OR REPLACE VIEW user_tags AS
 	SELECT UserInterest.idMember AS idMember, Tag.name AS name
 	FROM UserInterest INNER JOIN Tag ON Tag.id = UserInterest.idTag;
 
@@ -125,7 +166,7 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER delete_from_user_tags_view_trigger INSTEAD OF DELETE ON user_tags FOR EACH ROW	
 	EXECUTE PROCEDURE delete_from_user_tags_view();
 	
-CREATE VIEW model_tags AS
+CREATE OR REPLACE VIEW model_tags AS
 	SELECT ModelTag.idModel AS idModel, Tag.name AS name
 	FROM ModelTag INNER JOIN Tag ON Tag.id = ModelTag.idTag;
 	
@@ -169,6 +210,13 @@ CREATE TRIGGER delete_from_model_tags_view_trigger INSTEAD OF DELETE ON model_ta
 -----------------------
 
 -- Update model description --
-UPDATE TABLE Model SET description = :description WHERE id = :id;
+CREATE OR REPLACE FUNCTION set_model_description(modelId bigint, modelDescription varchar)
+RETURNS VOID AS $$
+	UPDATE Model SET description = $2 WHERE id = $1;
+$$ LANGUAGE SQL;
+
 -- Update member's about field --
-UPDATE TABLE Member SET about = :about WHERE id = :id;
+CREATE OR REPLACE FUNCTION set_member_about(memberId bigint, memberAbout varchar)
+RETURNS VOID AS $$
+	UPDATE Member SET about = $2 WHERE id = $1;
+$$ LANGUAGE SQL;
