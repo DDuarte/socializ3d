b5a49940562a09445716d5b0d0a703c2ed28a0e9
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-04-19 20:32:27 +0100

    Add getUserInfo function to users.php.

--- database/users.php
+++ database/users.php
@@ -13,3 +13,25 @@ function getUserHash($id)
     $result = $stmt->fetch();
     return $result['get_user_hash'];
 }
+
+function getUserInfo($id) {
+    global $conn;
+    $stmt = $conn->prepare("SELECT Member.name FROM Member WHERE Member.id = ?");
+    $stmt->execute(array($id));
+    $username = $stmt->fetchAll()[0];
+
+    $stmt = $conn->prepare("SELECT * FROM get_complete_friends_of_member(?)");
+    $stmt->execute(array($id));
+    $friends = $stmt->fetchAll();
+
+    $stmt = $conn->prepare("SELECT * FROM get_complete_groups_of_member(?)");
+    $stmt->execute((array($id)));
+    $groups = $stmt->fetchAll();
+
+    $result['userId'] = $id;
+    $result['username'] = $username;
+    $result['friends'] = $friends;
+    $result['groups'] = $groups;
+
+    return $result;
+}
