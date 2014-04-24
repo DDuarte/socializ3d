4a951ebec019ee2ddeb01041649a14619c371bce
migulorama <migulorama@gmail.com>
2014-04-24 22:29:00 +0100

    Stop redirecting to promotinal.html in index.

--- actions/indexAction.php
+++ actions/indexAction.php
@@ -1,27 +1,20 @@
 <?php
 
-class IndexHandler {
-    function get() {
-        if (!isset($_SESSION['id'])) {
-            global $BASE_URL;
-            header("Location: " . $BASE_URL . "promotional.html");
-        } else {
-            global $smarty;
-            global $BASE_DIR;
-            include($BASE_DIR . 'pages/common/header.php');
-            include($BASE_DIR . 'pages/catalogs/whats-hot.php');
-            include($BASE_DIR . 'pages/common/footer.php');
-        }
+class IndexHandler
+{
+    function get()
+    {
+        global $smarty;
+        global $BASE_DIR;
+        include($BASE_DIR . 'pages/common/header.php');
+        include($BASE_DIR . 'pages/catalogs/whats-hot.php');
+        include($BASE_DIR . 'pages/common/footer.php');
     }
 
-    function get_xhr() {
-        if (!isset($_SESSION['id'])) {
-            global $BASE_URL;
-            header("Location: " . $BASE_URL . "promotional.html");
-        } else {
-            global $smarty;
-            global $BASE_DIR;
-            include($BASE_DIR . 'pages/catalogs/whats-hot.php');
-        }
+    function get_xhr()
+    {
+        global $smarty;
+        global $BASE_DIR;
+        include($BASE_DIR . 'pages/catalogs/whats-hot.php');
     }
 }
