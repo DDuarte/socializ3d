b08181a11870faac217cee475f7ed8a3431a3caa
Duarte Duarte <dnpd.dd@gmail.com>
2014-04-25 06:15:55 +0100

    Delete server.bat and server.py
    They are no longer useful because we are using PayHighPrice now

--- server.py
+++
@@ -1,9 +0,0 @@
-import sys;
-import os;
-
-if sys.version_info[0] == 3:
-    os.system("start python -m http.server")
-else:
-    os.system("start python -m SimpleHTTPServer")
-    
-os.system("start /max http://localhost:8000")
