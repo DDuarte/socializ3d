48de7f5887a522ec0fde61836a5ac527eab85020
Luis <luisccleto@gmail.com>
2014-04-25 06:47:40 +0100

    Change register to login after creating account.

--- actions/members/register.php
+++ actions/members/register.php
@@ -32,5 +32,18 @@ try {
     exit;
 }
 
+$usr = getIdIfLoginCorrect($username, $password);
+
+if ($usr != false) {
+    $_SESSION['id'] = $usr['id'];
+    $_SESSION['isAdmin'] = $usr['isadmin'];
+    $_SESSION['username'] = $username;
+    $_SESSION['success_messages'][] = 'Login successful';
+} else {
+    //$_SESSION['error_messages'][] = 'Something went (very) wrong :/';
+    header("Location: " . $BASE_URL . "register.php");
+    exit;
+}
+
 $_SESSION['success_messages'][] = 'User registered successfully';
 header("Location: $BASE_URL");
