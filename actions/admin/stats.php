0391abf68912092bc5fb7b83388e416b70b97c63
Duarte Duarte <dnpd.dd@gmail.com>
2014-05-16 17:47:52 +0100

    Stats: also handle the date range in get (+get_xhr)
    because gnomo is weird

--- actions/admin/stats.php
+++ actions/admin/stats.php
@@ -2,8 +2,23 @@
 
 class StatisticsHandler
 {
+    function processDates($start, $end)
+    {
+        global $conn;
+        $stmt = $conn->prepare("SELECT * FROM get_counts_per_month_year(:startDate, :endDate)");
+        $stmt->execute(array(':startDate' => $start, ':endDate' => $end));
+        $result = $stmt->fetchAll();
+
+        return json_encode($result);
+    }
+
     function get()
     {
+        if (isset($_GET['startDate']) && isset($_GET['endDate'])) {
+            echo $this->processDates($_GET['startDate'], $_GET['endDate']);
+            return;
+        }
+
         global /** @noinspection PhpUnusedLocalVariableInspection */
         $smarty;
         global $BASE_DIR;
@@ -15,15 +30,7 @@ class StatisticsHandler
     function get_xhr()
     {
         if (isset($_GET['startDate']) && isset($_GET['endDate'])) {
-            $start = $_GET['startDate'];
-            $end = $_GET['endDate'];
-
-            global $conn;
-            $stmt = $conn->prepare("SELECT * FROM get_counts_per_month_year(:startDate, :endDate)");
-            $stmt->execute(array(':startDate' => $start, ':endDate' => $end));
-            $result = $stmt->fetchAll();
-
-            echo json_encode($result);
+            echo $this->processDates($_GET['startDate'], $_GET['endDate']);
             return;
         }
 
