67033acde6f6ae768cb1980cb8e7020d769e8cae
Duarte Duarte <dnpd.dd@gmail.com>
2014-05-16 17:26:31 +0100

    Escape html characters in comments POST

--- actions/models/comments.php
+++ actions/models/comments.php
@@ -14,7 +14,7 @@ class CommentsHandler {
         global $smarty;
 
         if (isset($_POST['content']) && strlen($_POST['content']) > 0)
-            insertComment(getLoggedId(), $modelId, $_POST['content']);
+            insertComment(getLoggedId(), $modelId, htmlspecialchars($_POST['content']));
 
         header('Location: ' . $_SERVER['HTTP_REFERER'] . '#tab_comments');
     }
