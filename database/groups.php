a8e304b95080854b58529a7f73d244715a47bbac
Duarte Duarte <dnpd.dd@gmail.com>
2014-05-16 10:26:42 +0100

    Add gallery to group pages

--- database/groups.php
+++ database/groups.php
@@ -2,8 +2,7 @@
 
 // include_once('users.php');
 
-function getGroup($id)
-{
+function getGroup($id) {
     global $conn;
     $stmt = $conn->prepare("SELECT * FROM get_group_profile(?, ?)");
     $stmt->execute(array(getLoggedId(), $id));
@@ -12,8 +11,18 @@ function getGroup($id)
     if ($result == false) return false;
 
     $result['id'] = $id;
+    $result['models'] = getGroupModels($id);
 
     return $result;
+}
+
+function getGroupModels($id) {
+    $loggedId = getLoggedId();
+
+    global $conn;
+    $stmt = $conn->prepare("SELECT *, (get_thumbnail_information(Model.id)).*, get_user_hash(Model.idAuthor) AS authorHash FROM Model JOIN get_group_models(1000, 0, :logged_id, :id) ON (Model.id = modelId)");
+    $stmt->execute(array(':id' => $id, ':logged_id' => $loggedId));
+    return $stmt->fetchAll();
 }
 
 function isGroupVisibleToMember($groupId, $memberId) {
