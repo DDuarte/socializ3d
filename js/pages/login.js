985c904a2d82560381049c7ba0a9a11cbf06d030
Duarte Duarte <dnpd.dd@gmail.com>
2014-06-08 00:25:31 +0100

    Update username register/login constraints

--- js/pages/login.js
+++ js/pages/login.js
@@ -14,9 +14,9 @@ $(document).ready(function() {
                         message: 'The username is required and cannot be empty'
                     },
                     stringLength: {
-                        min: 6,
-                        max: 30,
-                        message: 'The username must be more than 6 and less than 30 characters long'
+                        min: 3,
+                        max: 20,
+                        message: 'The username must be more than 3 and less than 20 characters long'
                     },
                     regexp: {
                         regexp: /^[a-zA-Z0-9_]+$/,
