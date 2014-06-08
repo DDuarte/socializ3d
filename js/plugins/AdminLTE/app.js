9ef84863717e60cc2030ba36872f93e4944403ae
Duarte Duarte <dnpd.dd@gmail.com>
2014-06-08 21:22:48 +0100

    Remove iCheck plugin leftovers

--- js/plugins/AdminLTE/app.js
+++ js/plugins/AdminLTE/app.js
@@ -227,16 +227,6 @@ $(function () {
     });
     ////\CUSTOM////
 
-    /*
-     * We are gonna initialize all checkbox and radio inputs to
-     * iCheck plugin in.
-     * You can find the documentation at http://fronteed.com/iCheck/
-     */
-    $("input[type='checkbox'], input[type='radio']").iCheck({
-        checkboxClass: 'icheckbox_minimal',
-        radioClass: 'iradio_minimal'
-    });
-
     hash_change();
 
     $('body').on('click', 'a.dynamic_load', load_page);
@@ -411,41 +401,6 @@ function change_layout() {
 
 }(jQuery));
 
-/*
- * TODO LIST CUSTOM PLUGIN
- * -----------------------
- * This plugin depends on iCheck plugin for checkbox and radio inputs
- */
-(function ($) {
-    "use strict";
-
-    $.fn.todolist = function (options) {
-        // Render options
-        var settings = $.extend({
-            //When the user checks the input
-            onCheck: function (ele) {
-            },
-            //When the user unchecks the input
-            onUncheck: function (ele) {
-            }
-        }, options);
-
-        return this.each(function () {
-            $('input', this).on('ifChecked', function (event) {
-                var ele = $(this).parents("li").first();
-                ele.toggleClass("done");
-                settings.onCheck.call(ele);
-            });
-
-            $('input', this).on('ifUnchecked', function (event) {
-                var ele = $(this).parents("li").first();
-                ele.toggleClass("done");
-                settings.onUncheck.call(ele);
-            });
-        });
-    };
-
-}(jQuery));
 
 /* CENTER ELEMENTS */
 (function ($) {
