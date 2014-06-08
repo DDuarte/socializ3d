cdf661f1dc52a03a6f0692927e645ec07017b72f
Luis <luisccleto@gmail.com>
2014-06-08 18:05:50 +0100

    Add check to prevent being redirected to a bad request page.

--- login.php
+++ login.php
@@ -4,8 +4,13 @@ include_once('config/init.php');
 
 if (isset($_SERVER['HTTP_REFERER'])) {
     if (substr_compare($_SERVER['HTTP_REFERER'], 'login.php', -strlen('login.php'), strlen('login.php')) != 0
-        && substr_compare($_SERVER['HTTP_REFERER'], 'register.php', -strlen('register.php'), strlen('register.php')) != 0) {
+        && substr_compare($_SERVER['HTTP_REFERER'], 'register.php', -strlen('register.php'), strlen('register.php')) != 0
+    ) {
         $_SESSION['PREV_HTTP_REFERER'] = $_SERVER['HTTP_REFERER'];
+    }
+
+    if (substr_compare($_SERVER['HTTP_REFERER'], 'forgotpass.php', -strlen('login.php'), strlen('login.php')) != 0) {
+        $_SESSION['PREV_HTTP_REFERER'] = $BASE_URL;
     }
 }
 
